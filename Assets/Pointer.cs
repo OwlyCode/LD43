e920d00138f9a61f43c99b5f6cc7c295b9c3a0b4
Tristan Maindron <tmaindron@gmail.com>
2018-12-02 20:35:36 +0100

    Finish touch on the UI

---
+++ Assets/Pointer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Click());
+    }
+
+    IEnumerator Click()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            transform.position = transform.position + new Vector3(-2, 0, 0);
+            transform.Rotate(0, 0, 2);
+            yield return new WaitForSeconds(0.1f);
+            transform.position = transform.position - new Vector3(-2, 0, 0);
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
